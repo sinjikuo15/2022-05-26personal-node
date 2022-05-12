8c162c5e2ca6d728124dce4e2772d9218a3cb4aa
sinjikuo15 <shin.kuo15@gmail.com>
2022-05-12 10:44:52 +0800

    3-4 製作 login 頁面並處理 POST request（第三方套件 body-parser）

--- app.js
+++ app.js
@@ -11,12 +11,16 @@ const express = require('express');
 
 
 ////////////////////////////////////////////////////////////
-
+const bodyParser = require('body-parser');
 const app = express();
 
 //middleware
 app.use(express.static(path.join(__dirname, 'public')));
 //抓取在public底下的資源的意思，靜態資源的關係所以需使用static
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 app.use((req, res, next) => {
     console.log('Hello!');
     next();
@@ -30,9 +34,19 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {//當中介軟體發送一個請求時，執行下面的程式    
     res.status(200).sendFile(path.join(__dirname, 'views', 'index.html')) //這行是使用express讓上面的做法直接導到index.html葉面去
 });
+
 app.get('/login', (req, res) => {
     
     res.status(200).sendFile(path.join(__dirname, 'views', 'login.html')) //這行是使用express讓上面的做法直接導到login.html葉面去
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } =req.body;
+    if (email && password ){
+        res.redirect('/') //當email跟password有資料 導頁到/頁面
+    }else{
+        console.log('欄位尚未填寫完成')
+    }
 });
 
 app.listen(3000, () => {
