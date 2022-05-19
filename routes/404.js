3f5469c7382d2712d21bb4e5714594b6f5967d92
sinjikuo15 <shin.kuo15@gmail.com>
2022-05-19 10:58:05 +0800

    5-1 重構 auth 路由 (分為 route, controller) 包含error shop

--- routes/404.js
+++ routes/404.js
@@ -1,17 +1,12 @@
 const express = require('express');
 
+const errorController = require('../controllers/404');
+
 ////////////////////////////////////////////////////////////
+
 
 const router = express.Router();
 
-router.get('*', (req, res) => {
-    //代表所有的網址都會導到404頁面去(只要前面沒有設定的頁面)，所以須放在最下面。
-    res.status(404).render('404', {
-        path:'*',
-        pageTitle:'404',
-        notFind: 'No find sorry :(',
-    });
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-});
+router.get('*', errorController.getError );
 
 module.exports = router;
